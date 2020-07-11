4bf63736e2183d4e471e7605b62cf434b4985dca
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-07-11 09:41:14 +0900

    JSONの格納方法を変更

--- src/chap01/__test__/statement.test.ts
+++ src/chap01/__test__/statement.test.ts
@@ -1,21 +1,17 @@
-import * as fs from 'fs'
 import { statement } from '../statement';
-
-const invoicesJson = fs.readFileSync('./data/invoices.json');
-const invoices: any[] = JSON.parse(invoicesJson.toString())
-const playsJson = fs.readFileSync('./data/plays.json');
-const plays: any[] = JSON.parse(playsJson.toString())
+import invoices from '../../data/chap01/invoices.json'
+import plays from '../../data/chap01/plays.json';
 
 describe('statement', () => {
-  it('期待しているテキストが帰って来るか。', () => {
-    let expectedText = 
+  it('期待しているテキストが返って来るか。', () => {
+    let expectedText =
       'Statement for BigCo\n' +
       '  Hamlet: $650.00 (55 seats)\n' +
       '  As You Like It: $580.00 (35 seats)\n' +
       '  Othello: $500.00 (40 seats)\n' +
       'Amount owed is $1,730.00\n' +
       'You earned 47 credits\n'
-    ;
-      expect(statement(invoices[0], plays)).toBe(expectedText);
+      ;
+    expect(statement(invoices[0], plays)).toBe(expectedText);
   });
 });
