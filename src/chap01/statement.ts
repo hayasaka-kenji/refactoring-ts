8a63164386ba521ecc3b08ace7710bc4334c9fbf
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-07-12 15:58:22 +0900

    関数の順番を書籍に合わせる。

--- src/chap01/statement.ts
+++ src/chap01/statement.ts
@@ -11,42 +11,6 @@ export function statement(invoice: Invoice, plays: Play): string {
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 
-  function amountFor(aPerformance: Performance) {
-    let result = 0;
-    switch (playFor(aPerformance).type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
-    }
-    return result;
-  }
-
-  // playの中のplayIDを返す。
-  function playFor(aPerformance: Performance) {
-    return plays[aPerformance.playID];
-  }
-
-  // ボリューム特典のポイントを加算
-  function volumeCreditsFor(aPerformance: Performance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    // 喜劇の時は十人につき、さらにポイントを加算
-    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
-    return result;
-  }
-
   function totalAmount() {
     let result = 0;
     for (let perf of invoice.performances) {
@@ -70,5 +34,41 @@ export function statement(invoice: Invoice, plays: Play): string {
         style: "currency", currency: "USD",
         minimumFractionDigits: 2
       }).format(aNumber / 100);
+  }
+
+  // ボリューム特典のポイントを加算
+  function volumeCreditsFor(aPerformance: Performance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    // 喜劇の時は十人につき、さらにポイントを加算
+    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
+
+  // playの中のplayIDを返す。
+  function playFor(aPerformance: Performance) {
+    return plays[aPerformance.playID];
+  }
+
+  function amountFor(aPerformance: Performance) {
+    let result = 0;
+    switch (playFor(aPerformance).type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+    }
+    return result;
   }
 }
