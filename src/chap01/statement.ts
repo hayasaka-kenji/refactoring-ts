24a261f6a05fe82bf9c294e434a046415a756136
hayasaka-kenji <kenji.hayasaka@imjp.co.jp>
2020-07-12 15:32:27 +0900

    format変数の削除、金額計算の移動。

--- src/chap01/statement.ts
+++ src/chap01/statement.ts
@@ -4,20 +4,20 @@ export function statement(invoice: Invoice, plays: Play): string {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat("en-US",
-    {
-      style: "currency", currency: "USD",
-      minimumFractionDigits: 2
-    }).format;
+  // const format = new Intl.NumberFormat("en-US",
+  //   {
+  //     style: "currency", currency: "USD",
+  //     minimumFractionDigits: 2
+  //   }).format;
 
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
 
     // 注文の内訳を出力
-    result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 
@@ -55,5 +55,14 @@ export function statement(invoice: Invoice, plays: Play): string {
     // 喜劇の時は十人につき、さらにポイントを加算
     if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
     return result;
+  }
+
+  // 金額のフォーマット
+  function usd(aNumber: number) {
+    return new Intl.NumberFormat("en-US",
+      {
+        style: "currency", currency: "USD",
+        minimumFractionDigits: 2
+      }).format(aNumber / 100);
   }
 }
